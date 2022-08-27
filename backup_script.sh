77adb73c5f765672e3d8424586f8877a1184a07c
Xpehutta <42118488+Xpehutta@users.noreply.github.com>
2022-08-27 15:29:07 +0300

    Update backup_script.sh

--- backup_script.sh
+++ backup_script.sh
@@ -1,12 +1,49 @@
-#!/bin/bash
-  
-BACKUPDIR=$1
-ZIPTYPE=$2
-BACKUPNAME=$3
+#!/usr/bin/env bash
 
+set -euo pipefail
 
 {
-tar --overwrite -cvf $BACKUPDIR/$BACKUPNAME.tar /
-$ZIPTYPE -f $BACKUPDIR/$BACKUPNAME.tar | openssl enc -aes-256-cbc -out $BACKUPNAME.dat
-} 1>/dev/null 2>>error.log
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -d|--dir)
+            input_dir="$2";
+            shift;;
+        -a|--algorithm)
+            algorithm="$2";
+            shift;;
+        -o|--output)
+            output_name="$2";
+            shift;;
+        *)
+            echo "Unknown parameter passed: $1" >&2;
+            exit 1;;
+    esac
+    shift
+done
 
+case $algorithm in
+  gzip)
+    algo="--gzip";;
+  bzip|bzip2)
+    algo="--bzip2";;
+  xz)
+    algo="--xz";;
+  lza)
+    algo="--compress";;
+  lzip)
+    algo="--lzip";;
+  lzma)
+    algo="--lzma";;
+  zstd)
+    algo="--zstd";;
+  *)
+    algo="";;
+esac
+
+if [ ! -d "$input_dir" ]; then
+    echo "Directory does not exist: ${input_dir}" >&2
+    exit 1
+fi
+
+tar cfv - $algo $input_dir | openssl enc -e -aes-256-cbc -out "${output_name}.dat"
+} 1>/dev/null 2>>error.log
