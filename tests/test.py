1fa269490f28c7e6c897787d3bd11bcc629572ad
Xpehutta <42118488+Xpehutta@users.noreply.github.com>
2022-09-05 11:43:57 +0300

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,1 +1,12 @@
+from src.main import main
 
+def test():
+    main_list = main(file_name='contacts_part.xz', directory='/test', return_fl = True)
+    compare_list = []
+    with open('/test/compare_result.txt') as f:
+        for line in f.readlines():
+            _l = line.split('\t')
+            compare_list.append([_l[0], int(_l[1]), int(_l[2].strip())])
+    
+    for _ in range(10):
+        assert main_list[_] == compare_list[_]
